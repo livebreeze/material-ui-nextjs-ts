f2598cbaa1fe357f80751247fbf62c989f546ecc
sean chen <livebreeze@gmail.com>
2024-05-31 10:21:54 +0800

    add ignore type

--- src/app/demo-ocr/page.tsx
+++ src/app/demo-ocr/page.tsx
@@ -1,28 +1,25 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Tesseract from 'tesseract.js';
+import React, { useState } from "react";
+import Tesseract from "tesseract.js";
 
 const OCRComponent = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [image, setImage] = useState(null);
 
-  const handleImageChange = (event: { target: { files: any[]; }; }) => {
+  const handleImageChange = (event: { target: { files: any[] } }) => {
     const file = event.target.files[0];
     if (file) {
+      // @ts-ignore
       setImage(URL.createObjectURL(file));
     }
   };
 
   const handleClick = () => {
     if (image) {
-      Tesseract.recognize(
-        image,
-        'eng',
-        {
-          logger: (m) => console.log(m),
-        }
-      ).then(({ data: { text } }) => {
+      Tesseract.recognize(image, "eng", {
+        logger: (m) => console.log(m),
+      }).then(({ data: { text } }) => {
         setText(text);
       });
     }
@@ -30,15 +27,18 @@ const OCRComponent = () => {
 
   return (
     <div className="ocr-container p-4">
-    <input type="file" onChange={handleImageChange} className="mb-4" />
-    <button onClick={handleClick} className="bg-blue-500 text-white px-4 py-2 rounded">
-      Convert to Text
-    </button>
-    <div className="mt-4">
-      <h3 className="font-bold">Extracted Text:</h3>
-      <p className="whitespace-pre-line">{text}</p>
+      <input type="file" onChange={handleImageChange} className="mb-4" />
+      <button
+        onClick={handleClick}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Convert to Text
+      </button>
+      <div className="mt-4">
+        <h3 className="font-bold">Extracted Text:</h3>
+        <p className="whitespace-pre-line">{text}</p>
+      </div>
     </div>
-  </div>
   );
 };
 
